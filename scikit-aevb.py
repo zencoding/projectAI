5a2e585334097e0a5a66099fe645dc1f1e8f5929
y0ast <joost@joo.st>
2014-01-30 01:00:27 +0100

    updated scikit version, just comments to go

--- scikit-aevb.py
+++ scikit-aevb.py
@@ -22,7 +22,7 @@ class AEVB:
         Number of binary hidden units for encoder.
 
     n_hidden_variables : int, optional
-    	The dimensionality of Z
+        The dimensionality of Z
 
     learning_rate : float, optional
         The learning rate for weight updates. It is *highly* recommended
@@ -37,18 +37,19 @@ class AEVB:
         during training.
 
     sampling_rounds : int, optional
-    	Number of sampling rounds done on the minibatch
+        Number of sampling rounds done on the minibatch
 
     continuous : boolean, optional
-    	Set what type of data the auto-encoder should model
+        Set what type of data the auto-encoder should model
 
     verbose : int, optional
         The verbosity level. The default, zero, means silent mode.
 
-    random_state : integer or numpy.RandomState, optional
-        A random number generator instance to define the state of the
-        random permutations generator. If an integer is given, it fixes the
-        seed. Defaults to the global numpy random number generator.
+    TO DO (NEEDS SCIKIT LEARN UTIL FILE)
+        random_state : integer or numpy.RandomState, optional
+            A random number generator instance to define the state of the
+            random permutations generator. If an integer is given, it fixes the
+            seed. Defaults to the global numpy random number generator.
 
     Attributes
     ----------
@@ -66,7 +67,9 @@ class AEVB:
     Arxiv, preprint. http://arxiv.org/pdf/1312.6114v6.pdf
     """
     
-    def __init__(self, n_components_decoder = 200, n_components_encoder = 200, n_hidden_variables = 20, learning_rate=0.01,batch_size = 100, n_iter = 10, sampling_rounds = 1, continuous = False, verbose = False, random_state = None):
+    def __init__(self, n_components_decoder = 200, n_components_encoder = 200, 
+            n_hidden_variables = 20, learning_rate=0.01,batch_size = 100, 
+            n_iter = 10, sampling_rounds = 1, continuous = False, verbose = False):
         self.n_components_decoder = n_components_decoder
         self.n_components_encoder= n_components_encoder
         self.n_hidden_variables = n_hidden_variables
@@ -76,13 +79,18 @@ class AEVB:
         self.n_iter = n_iter
         self.sampling_rounds = sampling_steps
         self.verbose = verbose
-        self.random_state = random_state
 
         self.continuous = continuous
 
     def _initParams(self,dimX):
-        """Create all weight and bias parameters with the right dimensions"""
-    	sigmaInit = 0.01
+        """Create all weight and bias parameters with the right dimensions
+
+        Parameters
+        ----------
+        dimX : scalar
+            The dimensionality of the input data X
+        """
+        sigmaInit = 0.01
         W1 = np.random.normal(0,self.sigmaInit,(self.n_components_encoder,dimX))
         b1 = np.random.normal(0,self.sigmaInit,(self.n_components_encoder,1))
 
@@ -105,11 +113,18 @@ class AEVB:
             self.params = [W1,W2,W3,W4,W5,W6,b1,b2,b3,b4,b5,b6]
 
     def _initH(self,miniBatch):
+        """Initialize H for AdaGrad
+
+        Parameters
+        ----------
+
+        miniBatch: array-like, shape (batch_size, n_features)
+            The data to use for computing gradients
+        """
         self.h = [0.01] * len(self.params)
         totalGradients = self.getGradients(miniBatch)
         for i in xrange(len(totalGradients)):
             self.h[i] += totalGradients[i]*totalGradients[i]
-
 
     def _createGradientFunctions(self):
         #Create the Theano variables
@@ -123,104 +138,109 @@ class AEVB:
         else:   
             h_encoder = T.tanh(T.dot(W1,x) + b1)
 
-        mu = T.dot(W2,h_encoder) + b2
-        sigma = T.exp(0.5*(T.dot(W3,h_encoder) + b3))
+        mu_encoder = T.dot(W2,h_encoder) + b2
+        log_sigma_encoder = 0.5*(T.dot(W3,h_encoder) + b3)
 
         #Find the hidden variable z
-        z = mu + sigma*eps
+        z = mu_encoder + T.exp(log_sigma_encoder)*eps
 
-        #Set up likelihood
-        h_decoder = T.tanh(T.dot(W4,z) + b4)
-
+        #Set up decoding layer
         if self.continuous:
-            decoder_mu = T.nnet.sigmoid(T.dot(W5,h_decoder) + b5)
-            decoder_sigma = T.exp(0.5*(T.dot(W6,h_decoder) + b6))
-            logpxz = T.sum(-(0.5 * np.log(np.pi) + T.log(decoder_sigma)) - 0.5 * ((x - decoder_mu) / decoder_sigma)**2)
-        else:
-            y = T.nnet.sigmoid(T.dot(W5,h_decoder) + b5)
-            logpxz = -T.nnet.binary_crossentropy(y,x).sum()
-
-        #Set up q 
-        logqzx = T.sum(-(z - mu)**2/(2.*sigma**2) - 0.5 * T.log(2. * np.pi * sigma**2))
-
-        #Compute prior
-        logpz = T.sum(-(z**2)/2 - 0.5 * np.log(2 * np.pi))
-
-        #Define lowerbound
-        logp = logpxz + logpz - logqzx
-
-        #Compute all the gradients
-        if self.continuous:
+            h_decoder = T.nnet.softplus(T.dot(W4,z) + b4)
+            mu_decoder = T.nnet.sigmoid(T.dot(W5,h_decoder) + b5)
+            log_sigma_decoder = 0.5*(T.dot(W6,h_decoder) + b6)
+            logpxz = T.sum(-(0.5 * np.log(2 * np.pi) + log_sigma_decoder) - 0.5 * ((x - mu_decoder) / T.exp(log_sigma_decoder))**2)
+            rest = 0.5* T.sum(1 + 2*log_sigma_encoder - mu_encoder**2 - T.exp(2*log_sigma_encoder) )
+            logp = logpxz + rest
             gradvariables = [W1,W2,W3,W4,W5,W6,b1,b2,b3,b4,b5,b6]
         else:
+            h_decoder = T.tanh(T.dot(W4,z) + b4)
+            y = T.nnet.sigmoid(T.dot(W5,h_decoder) + b5)
+            logpxz = -T.nnet.binary_crossentropy(y,x).sum()
+            logqzx = T.sum(-(0.5 * np.log(2 * np.pi) + log_sigma_encoder) - 0.5 * ((z - mu_encoder)/T.exp(log_sigma_encoder))**2)
+            logpz = T.sum(-0.5*(z**2) - 0.5 * np.log(2 * np.pi))
+            logp = logpxz + logpz - logqzx
             gradvariables = [W1,W2,W3,W4,W5,b1,b2,b3,b4,b5]
 
+        #Compute all the gradients
         derivatives = T.grad(logp,gradvariables)
 
         #Add the lowerbound so we can keep track of results
         derivatives.append(logp)
 
-        self.gradientfunction = th.function(gradvariables + [x,eps], derivatives, on_unused_input='ignore')
+        gradientfunction = th.function(gradvariables + [x,eps], derivatives, on_unused_input='ignore')
+        lowerboundfunction = th.function(gradvariables + [x,eps], logp, on_unused_input='ignore')
+
+        return (gradientfunction,lowerboundfunction)
 
     def _getLowerBound(self,data):
         lowerbound = 0
         [N,dimX] = data.shape
-        batches = np.linspace(0,N,N/self.batch_size+1)
+        batches = np.arange(0,N,self.batch_size)
+        if batches[-1] != N:
+            batches = np.append(batches,N)
 
         for i in xrange(0,len(batches)-2):
-            e = np.random.normal(0,1,[self.dimZ,self.batch_size])
             miniBatch = data[batches[i]:batches[i+1]]
-            gradients = self.gradientfunction(*(self.params),x=miniBatch.T,eps=e)
-            lowerbound += gradients[10]
+            e = np.random.normal(0,1,[self.dimZ,miniBatch.shape[0]])
+            lowerbound += self.lowerboundfunction(*(self.params),x=miniBatch.T,eps=e)
 
             return lowerbound/N
 
 
-    def _getGradients(self,miniBatch):
+    def _getGradients(self,miniBatch,gradientfunction):
         totalGradients = [0] * len(self.params)
         for l in xrange(self.L):
-            e = np.random.normal(0,1,[self.dimZ,self.batch_size])
-            gradients = self.gradientfunction(*(self.params),x=miniBatch,eps=e)
-            self.lowerbound += gradients[-1]
+            e = np.random.normal(0,1,[self.dimZ,miniBatch.shape[1]])
+            gradients = gradientfunction(*(self.params),x=miniBatch,eps=e)
 
-            for i in xrange(len(totalGradients)):
+            for i in xrange(len(self.params)):
                 if np.isnan(np.sum(gradients[i])):
                     print "The gradients contain nans, that cannot be right"
                     exit()
 
                 totalGradients[i] += gradients[i]
 
-        return totalGradients
+        return (totalGradients,gradients[-1])
 
-    def _updateParams(self,totalGradients,N):
+    def _updateParams(self,totalGradients,N,current_batch_size):
         for i in xrange(len(self.params)):
             self.h[i] += totalGradients[i]*totalGradients[i]
-            prior = 0.5*self.params[i]*(i<5)
+            if i < 5 or (i < 6 and len(self.params) == 12):
+                prior = 0.5*self.params[i]
+            else:
+                prior = 0
 
-            #Include adagrad, include prior for weights
-            self.params[i] = self.params[i] + (self.learning_rate)/np.sqrt(self.h[i]) * (totalGradients[i] - prior*(self.batch_size/N))
+            self.params[i] += self.learning_rate/np.sqrt(self.h[i]) * (totalGradients[i] - prior*(current_batch_size/N))
 
-     def fit(self,data):
-     	[N,dimX] = data.shape
-     	self._initParams(self,dimX)
+    def fit(self,data,iterations):
+        [N,dimX] = data.shape
+        self._initParams(self,dimX)
+        list_lowerbound = np.array([])
 
-        batches = np.linspace(0,N,N/self.batch_size+1)
+        (gradientfunction,lowerboundfunction) = self._createGradientFunctions()
+
+        batches = np.arange(0,N,self.batch_size)
+        if batches[-1] != N:
+            batches = np.append(batches,N)
 
         for i in xrange(10):
-        	miniBatch = data[batches[i]:batches[i+1]]
-     		self._initH(self,miniBatch)
-
-
-        for i in xrange(0,len(batches)-2):
-            totalGradients = self._getGradients(miniBatch.T)
-            self._updateParams(totalGradients,N)
+            miniBatch = data[batches[i]:batches[i+1]]
+            self._initH(self,miniBatch)
+        for i in xrange(iterations):
+            iteration_lowerbound = 0
+            for j in xrange(0,len(batches)-2):
+                totalGradients,lowerbound = self._getGradients(miniBatch.T,gradientfunction)
+                iteration_lowerbound += lowerbound
+                self._updateParams(totalGradients,N)
+            list_lowerbound = np.append(list_lowerbound,iteration_lowerbound)
 
     def transform(self,data):
-    	if self.continuous:
-    		return np.log(1+np.exp(data.dot(self.params[0].T) + params[6].T))
-    	else:
-    		return np.tanh(data.dot(self.params[0].T) + self.params[5].T)
+        if self.continuous:
+            return np.log(1+np.exp(data.dot(self.params[0].T) + params[6].T))
+        else:
+            return np.tanh(data.dot(self.params[0].T) + self.params[5].T)
 
-    def fit_transform(self,data):
-    	self.fit(data)
-    	return self.transform(data)
+    def fit_transform(self,data,iterations):
+        self.fit(data.iterations)
+        return self.transform(data)
