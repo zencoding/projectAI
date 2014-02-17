bb7e1b3cc70c1531707405ba89ed10d37534b1df
y0ast <joost@joo.st>
2014-02-17 20:46:11 +0100

    fix scikit and report fixes

--- scikit implementation/scikitaevb.py
+++ scikit implementation/scikitaevb.py
@@ -117,26 +117,27 @@ class AEVB:
         for key in self.params:
             self.h[key] = 0.01
 
-    def _initH(self,miniBatch):
+    def _initH(self,minibatch):
         """Initialize H for AdaGrad
 
         Parameters
         ----------
 
-        miniBatch: array-like, shape (batch_size, n_features)
+        minibatch: array-like, shape (batch_size, n_features)
             The data to use for computing gradients
         """
 
-        e = np.random.normal(0,1,[self.n_hidden_variables,miniBatch.shape[0]])
-        gradients,lowerbound = self._computeGradients(miniBatch.T,e)
+        e = np.random.normal(0,1,[self.n_hidden_variables,minibatch.shape[0]])
+        gradients,lowerbound = self._computeGradients(minibatch.T,e)
+
         for key in gradients:
-            self.h[key] += totalGradients[key]**2
+            self.h[key] += gradients[key]**2
 
     def _computeGradients(self,x,eps):
         W1, W2, W3, W4, W5 = self.params["W1"], self.params["W2"], self.params["W3"], self.params["W4"], self.params["W5"]
         b1, b2, b3, b4, b5 = self.params["b1"], self.params["b2"], self.params["b3"], self.params["b4"], self.params["b5"]
 
-        sigmoid = lambda x: 1/1+np.exp(-x)
+        sigmoid = lambda x: 1/(1+np.exp(-x))
 
         h_encoder = np.tanh(W1.dot(x) + b1)
         mu_encoder = W2.dot(h_encoder) + b2
@@ -147,74 +148,139 @@ class AEVB:
         h_decoder = np.tanh(W4.dot(z) + b4)
         y = sigmoid(W5.dot(h_decoder) + b5)
 
-        #z: 20x100
+        logpxz = np.sum(x * np.log(y) + (1 - x) * np.log(1-y))
+        KLD = 0.5 * np.sum(1 + 2*log_sigma_encoder - mu_encoder**2 - np.exp(2*log_sigma_encoder)) 
+        logp = logpxz + KLD
+
+        #now sum over batch
+        h_encoder = np.sum(h_encoder,1,keepdims=True)
+        h_decoder = np.sum(h_decoder,1,keepdims=True)
+        z = np.sum(z,1,keepdims=True)
+        #log_sigma_encoder staat nu later, maar kan evt hier. y staat ook later want we willen x niet middelen (toch?)
+
+        #z: 20x1
         #W1: 400x784
         #W2: 20x400
         #W3: 20x400
         #W4: 400x20
         #W5: 784x400
-        #h_encoder: 400x100
+        #h_encoder: 400x1
 
         #784x100
-        #1x1
-        dp_dy = np.sum((x/y - (x - 1)/(y - 1)), keepdims=True)
-        #400x100
-        dy_dHd = W5.T.dot(sigmoid(W5.dot(h_decoder) + b5) * (1 - sigmoid(W5.dot(h_decoder) + b5)))
-        #100x20
-        dHd_dz = -(np.tanh(W4.dot(z) + b4)**2 - 1).T.dot(W4)
-        #1x1
-        dz_dmue = 1
-        #20x100
-        dz_dlogsige = eps * np.exp(log_sigma_encoder)
+        #784x1
+        dp_dy = np.sum((x/y - (x - 1)/(y - 1)),1, keepdims=True)
+        #784x400
+        dy_dHd = np.multiply(W5,(sigmoid(W5.dot(h_decoder) + b5) * (1 - sigmoid(W5.dot(h_decoder) + b5))))
         #400x20
+        dHd_dz = np.multiply(-(np.tanh(W4.dot(z) + b4)**2 - 1),W4)
+        #20x1
+        dz_dmue = np.ones_like(z)
+        #20x1
+        #kunnen we niet net zo goed 1 keer noise samplen met kleinere variantie??
+        dz_dlogsige = np.sum(eps * np.exp(log_sigma_encoder),1,keepdims=True) 
+        #20x400
         dmue_dHe = W2
-        #400x20
-        dlogsige_dHe = (0.5 * W3).T
+        #20x400
+        dlogsige_dHe = (0.5 * W3)
+
+        # dDKL_dlogsige = 1 - np.exp(2*log_sigma_encoder)
+        # dDKL_dmue = -mu_encoder
+
+        #20x400
+        dmue_dW2 = h_encoder.dot(np.ones((1,20))).T
+        #400x784
+        dHe_dW1 = (-(np.tanh(W1.dot(x) + b1)**2 - 1).dot(x.T))
 
         #Add h_decoder transposed at end
         dp_dW5 = dp_dy * ((sigmoid(W5.dot(h_decoder) + b5) * (1 - sigmoid(W5.dot(h_decoder) + b5))).dot(h_decoder.T))
         dp_db5 = dp_dy * sigmoid(W5.dot(h_decoder) + b5) * (1 - sigmoid(W5.dot(h_decoder) + b5))
-        print dp_dW5.shape, W5.shape
-        print dp_db5.shape, b5.shape #need to sum here?
-
-        dp_dHd = dp_dy * dy_dHd
         
-        #Here suddenly need element wise with broadcasting, INCOSISTENT AAAAAH
-        dp_dW4 = np.sum(dp_dHd, axis=1,keepdims=True)*(-(np.tanh(W4.dot(z) + b4)**2 - 1).dot(z.T))
-        dp_db4 = np.sum(dp_dHd*(1 - np.tanh(W4.dot(z) + b4)**2),1,keepdims = True)
-        print W4.shape,dp_dW4.shape
-        print b4.shape,dp_db4.shape
-
+        #1x400
+        dp_dHd = dp_dy.T.dot(dy_dHd)
+        
+        #400x20
+        dp_dW4 = dp_dHd.T * (-(np.tanh(W4.dot(z) + b4)**2 - 1).dot(z.T))
+        # print W4.shape, dp_dW4.shape
+        
+        #400x1
+        dp_db4 = dp_dHd.T*(1 - np.tanh(W4.dot(z) + b4)**2)
+        # print b4.shape, dp_db4.shape
+        
+        #1x20
         dp_dz = dp_dHd.dot(dHd_dz)
-
-        #dz_dmue is 1
-        dp_dmue = dp_dz.dot(1)
-
-        dp_dW2 = dp_dmue * np.sum(h_encoder,axis=1,keepdims=True)
-        #dmue_db2 is 1
+        # print '(1, 20)', dp_dz.shape
+        
+        #20x1
+        dp_dmue = dp_dz.T*dz_dmue
+        # print '(20, 1)', dp_dmue.shape
+        
+        #20x400
+        dp_dW2 = dp_dmue*dmue_dW2
+        # print W2.shape, dp_dW2.shape
+        
+        #dmue_db2 is 1, 20x1
         dp_db2 = dp_dmue
+        # print '(20,1)',dp_db2.shape
 
-        #Part one of z, dz_dmue is 1
-        dz_dHe = dmue_dHe
-        dz_dW1 = dz_dHe.dot(-(np.tanh(W1.dot(x) + b1)**2 - 1).dot(x.T))
-        dz_db1 = dz_dHe.dot(1 - np.tanh(W1.dot(x) + b1)**2)
+        #Part one of z, dz_dmue is 1     
 
-        dp_dlogsige = dp_dz.dot(dz_dlogsige)
+        # print W1.shape, dHe_dW1.shape
+        
+        #20x1
+        dp_dmue = dp_dz.T * dz_dmue
+        # print '(20,1)',dp_dmue.shape
+        
+        #1x400
+        dp_dHe = dp_dmue.T.dot(dmue_dHe)
+        # print '(1x400)', dp_dHe.shape
+        
+        #400x784
+        dp_dW1_1 = dp_dHe.T*dHe_dW1
+        # print W1.shape, dp_dW1_1.shape
+        
+        #400x1
+        dHe_db1 = np.sum(1 - np.tanh(W1.dot(x) + b1)**2,1,keepdims=True)
+        # print '(400,1)', dHe_db1.shape
+        
+        #400x1
+        dp_db1_1 = dp_dHe.T*dHe_db1
+        # print '(400,1)',dp_db1_1.shape 
+        
+        #20x1
+        dp_dlogsige = dp_dz.T*dz_dlogsige
+        # print '(20,1)', dp_dlogsige.shape
+        
+        #20x1
+        dp_dW3 = dp_dlogsige.dot(0.5 * h_decoder.T)
+        # print W3.shape, dp_dW3.shape
 
-        dp_dW3 = dp_dlogsige * np.sum(0.5 * h_decoder, axis=1,keepdims=True)
-        dp_db3 = dp_dlogsige.dot(0.5)
+        dp_db3 = dp_dlogsige*0.5
+        # print b3.shape, dp_dlogsige.shape
 
         #Part two of z 
-        dhd_dHe_2 = dz_dlogsige.T.dot(dmue_dHe)
-        dz_dW1_2 = dhd_dHe_2.dot(-(np.tanh(W1.dot(x) + b1)**2 - 1).dot(x.T))
-        dz_db1_2 = dhd_dHe_2.dot(1 - np.tanh(W1.dot(x) + b1)**2)
+        #400x1
+        dp_dHe_2 = dlogsige_dHe.T.dot(dp_dlogsige)
+        # print '(400,1)', dp_dHe_2.shape
+
+        #400x784
+        dp_dW1_2 = dp_dHe_2*dHe_dW1
+        # print W1.shape, dp_dW1_2.shape
+
+
+        dp_db1_2 = dp_dHe_2*dHe_db1
+        # print b1.shape, dp_db1_2.shape
 
         #weird dimension mismatch, there is something really wrong
-        dp_dW1 = dp_dz.dot(dz_dW1 + dz_dW1_2)
-        dp_db1 = dp_dz.dot(dz_db1 + dz_db1_2)
+        #400x784
+        dp_dW1 = dp_dW1_1 + dp_dW1_2
+        # print W1.shape, dp_dW1.shape
+
+        #400x1
+        dp_db1 = dp_db1_1 + dp_db1_2
+        # print b1.shape, dp_db1.shape
 
         return {"W1": dp_dW1, "W2": dp_dW2, "W3": dp_dW3, "W4": dp_dW4, "W5": dp_dW5,
-        "b1": dp_db1, "b2": dp_db2, "b3": dp_db3, "b4": dp_db4, "b5": dp_db5}
+        "b1": dp_db1, "b2": dp_db2, "b3": dp_db3, "b4": dp_db4, "b5": dp_db5}, logp
         
 
     def _computeLB(self,data, eps):
@@ -239,8 +305,8 @@ class AEVB:
             batches = np.append(batches,N)
 
         for i in xrange(0,len(batches)-2):
-            miniBatch = data[batches[i]:batches[i+1]]
-            e = np.random.normal(0,1,[self.dimZ,miniBatch.shape[0]])
+            minibatch = data[batches[i]:batches[i+1]]
+            e = np.random.normal(0,1,[self.dimZ,minibatch.shape[0]])
             lowerbound += self._computeLB(data,eps)
 
             return lowerbound/N
@@ -248,27 +314,29 @@ class AEVB:
 
     def _updateParams(self,minibatch,N):
         for l in xrange(self.sampling_rounds):
-            e = np.random.normal(0,1,[self.n_hidden_variables,miniBatch.shape[0]])
-            gradients = self._computeGradients(miniBatch.T,eps)
+            e = np.random.normal(0,1,[self.n_hidden_variables,minibatch.shape[0]])
+            gradients,lowerbound = self._computeGradients(minibatch.T,e)
 
-            if not total_gradients:
+            if 'total_gradients' not in locals():
                 total_gradients = gradients
             else:
                 for key in gradients:
-                    if np.isnan(gradients[key]):
+                    if np.isnan(np.sum(gradients[key])):
                         print "The gradient " + key + " contain nans, that cannot be right"
                         exit()
 
-                    totalGradients[key] += gradients[key]
+                    total_gradients[key] += gradients[key]
 
         for key in self.params:
-            self.h[key] += totalGradients[key]**2
+            self.h[key] += total_gradients[key]**2
             if "W" in key:
                 prior = 0.5*self.params[key]
             else:
                 prior = 0
 
-            self.params[key] += self.learning_rate/np.sqrt(self.h[key]) * (totalGradients[key] - prior*(minibatch.shape[0]/N))
+            self.params[key] += self.learning_rate/np.sqrt(self.h[key]) * (total_gradients[key] - prior*(minibatch.shape[0]/N))
+
+        return lowerbound
 
     def fit(self,data):
         [N,dimX] = data.shape
@@ -282,19 +350,19 @@ class AEVB:
         if self.verbose:
             print "Initialize H"
         for i in xrange(10):
-            miniBatch = data[batches[i]:batches[i+1]]
-            self._initH(miniBatch)
+            minibatch = data[batches[i]:batches[i+1]]
+            self._initH(minibatch)
 
         for i in xrange(self.n_iter):
             if self.verbose:
                 print "iteration:", i
-            # iteration_lowerbound = 0
+            iteration_lowerbound = 0
             for j in xrange(0,len(batches)-2):
-                # iteration_lowerbound += lowerbound
-                self._updateParams(miniBatch, N)
-            # print iteration_lowerbound/N
-            # list_lowerbound = np.append(list_lowerbound,iteration_lowerbound/N)
-        # return list_lowerbound
+                lowerbound = self._updateParams(minibatch, N)
+                iteration_lowerbound += lowerbound
+            print iteration_lowerbound/N
+            list_lowerbound = np.append(list_lowerbound,iteration_lowerbound/N)
+        return list_lowerbound
 
     def transform(self,data):
         if self.continuous:
--- train/trainchinese.py
+++ train/trainchinese.py
@@ -9,7 +9,7 @@ Otto Fabius - 5619858
 #Example: python -m train.trainchinese -s chinese.npy
 
 import aevb
-from loadsave import load_notest, save_notest,load_filtered_chinese
+from loadsave import load, save,load_filtered_chinese
 import numpy as np
 import argparse
 
@@ -20,8 +20,8 @@ parser.add_argument("-s", "--save", help="Specify file to save params", default 
 args = parser.parse_args()
 
 
-dimZ = 200
-HU_decoder = 500
+dimZ = 1000
+HU_decoder = 400
 HU_encoder = HU_decoder
 
 batch_size = 100
@@ -29,7 +29,14 @@ L = 1
 learning_rate = 0.01
 
 
-(data, t_train) = load_filtered_chinese()
+(data, labels) = load_filtered_chinese()
+x_train = data[:10000]
+t_train = labels[:10000]
+
+x_test = data[10000:]
+t_test = labels[10000:]
+
+
 [N, dimX] = data.shape
 encoder = aevb.AEVB(HU_decoder, HU_encoder, dimX, dimZ, batch_size, L, learning_rate)
 
@@ -39,20 +46,26 @@ encoder.createGradientFunctions()
 print "Initializing weights and biases"
 if args.params:
     print "Loading params from: {0}".format(args.params)
-    encoder.params, encoder.h, lowerbound = load_notest(args.params)
+    encoder.params, encoder.h, lowerbound, testlowerbound = load_test(args.params)
 else:
     encoder.initParams()
     for i in xrange(0, 10):
-        encoder.initH(data[batch_size*i:batch_size*(i+1)].T)
+        encoder.initH(x_train[batch_size*i:batch_size*(i+1)].T)
     lowerbound = np.array([])
+    testlowerbound = np.array([])
+
 
 for j in xrange(20000):
     print 'Iteration:', j
     encoder.lowerbound = 0
-    encoder.iterate(data)
+    encoder.iterate(x_train)
     print encoder.lowerbound/N
     lowerbound = np.append(lowerbound, encoder.lowerbound/N)
 
+    if j % 5 == 0:
+        print "Calculating test lowerbound"
+        testlowerbound = np.append(testlowerbound,encoder.getLowerBound(x_test))
+
     if args.save:
         print "Saving params"
-        save_notest(args.save, encoder.params, encoder.h, lowerbound)
+        save(args.save,encoder.params,encoder.h,lowerbound,testlowerbound)
