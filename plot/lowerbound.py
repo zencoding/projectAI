59067de98e54850a184fdb65ade7848c503da3e8
y0ast <joost@joo.st>
2014-02-25 21:00:06 +0100

    oops forgot to add

--- plot/lowerbound.py
+++ plot/lowerbound.py
@@ -14,7 +14,7 @@ datatest = np.load('testlowerboundchinese.npy')
 xtest = np.arange(0,datatest.shape[0]*50000,50000)
 plt.plot(xtest,datatest, linewidth = 3)
 
-plt.axis((100000,10000000,-700,-350))
+plt.axis((100000,20000000,-700,-350))
 matplotlib.rcParams.update({'font.size': 22})
 plt.title('Chinese Characters')
 plt.legend(('LB of train set', 'LB of test set'),loc=4,prop={'size':20})
